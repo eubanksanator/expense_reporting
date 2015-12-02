2fb788e2ecb2b12d211f5f314b531bc508b269f1
Travis E <teubanks@tutelaholding.com>
2015-12-02 10:19:06 -0600

    delets migrations

--- db/migrate/20151201143821_add_business_client_id_to_entertain_expense_explanatin.rb
+++
@@ -1,5 +0,0 @@
-class AddBusinessClientIdToEntertainExpenseExplanatin < ActiveRecord::Migration
-  def change
-    add_column :entertain_expense_explanations, :business_client_id, :integer
-  end
-end
--- db/migrate/20151201154417_drop_entertain_expense_explanations.rb
+++
@@ -1,5 +0,0 @@
-class DropEntertainExpenseExplanations < ActiveRecord::Migration
-  def change
-    drop_table :entertain_expense_explanations
-  end
-end
