f20620d251db948f68a56c7947a04e5e18eeb6bd
Travis E <teubanks@tutelaholding.com>
2015-12-04 10:13:37 -0600

    no errors good with devise and receipts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201154417) do
+ActiveRecord::Schema.define(version: 20151204154851) do
 
   create_table "business_clients", force: :cascade do |t|
     t.date     "occurence"
@@ -66,6 +66,12 @@ ActiveRecord::Schema.define(version: 20151201154417) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "receipts", force: :cascade do |t|
+    t.string   "doc_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "travel_costs", force: :cascade do |t|
     t.date     "occurence"
     t.decimal  "cost"
@@ -80,8 +86,21 @@ ActiveRecord::Schema.define(version: 20151201154417) do
     t.string   "last_name"
     t.string   "department_name"
     t.string   "phone_number"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
